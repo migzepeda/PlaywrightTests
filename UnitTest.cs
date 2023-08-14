f08d0a0ad61488b5d315ebccf82689ea70b701fa
Miguel Zepeda <MZepeda@opentechalliance.com>
2023-08-14 14:30:46 -0700

    Added some more basic tests using our company pages.

--- UnitTest.cs
+++ UnitTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
@@ -43,7 +44,7 @@ public class UnitTest : PageTest
     {
         return new BrowserNewContextOptions()
         {
-            ColorScheme = ColorScheme.Light,
+            ColorScheme = ColorScheme.Dark,
             ViewportSize = new()
             {
                 Width = 1920,
@@ -51,5 +52,58 @@ public class UnitTest : PageTest
             },
             BaseURL = "https://github.com",
         };
+    }
+
+    [TestMethod]
+    public async Task HomepageHasOpenTechAllianceInTitleAndLoginLinkLinkingToLoginPage()
+    {
+        await Page.GotoAsync("https://opentechalliance.com/");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("OpenTech Alliance, Inc."));
+
+        // Create a locator
+        var login = Page.GetByRole(AriaRole.Link, new() { Name = "Login" });
+
+        // Expect an attribute "to be strictly equal" to the value.
+        await Expect(login).ToHaveAttributeAsync("href", "https://opentechalliance.com/customer-login-page/");
+
+        // Click the login link.
+        await login.ClickAsync();
+
+        // Expects the URL to contain customer login page.
+        await Expect(Page).ToHaveURLAsync(new Regex(".*customer-login-page/"));
+    }
+
+    [TestMethod]
+    public async Task DevPortalCanBeAccessedThroughHomepage()
+    {
+        // Go to OpenTech Alliance homepage.
+        await Page.GotoAsync("https://opentechalliance.com/");
+
+        // Create a locator for login.
+        var login = Page.GetByRole(AriaRole.Link, new() { Name = "Login" });
+
+        // Expect an attribute "to be strictly equal" to the value in the login locator.
+        await Expect(login).ToHaveAttributeAsync("href", new Regex("/customer-login-page/"));
+
+        // Click the login link.
+        await login.ClickAsync();
+
+        // Get popup after a specific action (e.g., click)
+        var popup = await Page.RunAndWaitForPopupAsync(async () =>
+        {
+            // Create a locator for the Developer API Portal, it should be the 4th Login link on the page
+            var devPortal = Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).Nth(3);
+
+            // Click the devPortal link.
+            await devPortal.ClickAsync();
+        });
+        // Wait for popup to load
+        await popup.WaitForLoadStateAsync();
+
+        // Expects the popup to contain OpenTech Alliance API Reference in the title
+        await Expect(popup).ToHaveTitleAsync(new Regex("OpenTech Alliance API Reference"));
+
     }
 }
